0db310858d794f69a321783a12133a1ec0a59e34
GoldenUser <wnajjar00@bk.ru>
2023-08-26 14:31:46 +0200

    Model aangepast naar een cleaner code. OrderController Toegevoegd en moet de functinliteiten invullen

---
+++ SmoothBoard.NET/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmoothBoard.NET.Models;
+
+namespace SmoothBoard.NET.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Creëer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Creëer(OrderModel order)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.SuccessMessage = "Bestelling succesvol aangemaakt!";
+            }
+
+            return View(order);
+        }
+    }
+}
--- SmoothBoard.NET/Models/Opdrachtgever.cs
+++ SmoothBoard.NET/Models/Opdrachtgever.cs
@@ -1,21 +1,41 @@
-﻿namespace SmoothBoard.NET.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmoothBoard.NET.Models
 {
     public class Opdrachtgever
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
 
-        public string name { get; set; }
+        [Required(ErrorMessage = "Naam is verplicht.")]
+        public string Naam { get; set; }
 
-        public string adres { get; set; }
+        [Required(ErrorMessage = "Adres is verplicht.")]
+        public string Adres { get; set; }
 
-        public int telefoonnummer { get; set; }
+        [Required(ErrorMessage = "Telefoonnummer is verplicht.")]
+        [Display(Name = "Telefoonnummer")]
+        public string Telefoonnummer { get; set; }
 
-        public string email { get; set; }
+        [Required(ErrorMessage = "E-mail is verplicht.")]
+        [EmailAddress(ErrorMessage = "Voer een geldig e-mailadres in.")]
+        public string Email { get; set; }
 
-        public DateTime brengDatum { get; set; }
+        [Required(ErrorMessage = "Brengdatum is verplicht.")]
+        [DataType(DataType.Date)]
+        public DateTime BrengDatum { get; set; }
 
+        [Required(ErrorMessage = "Ophaaldatum is verplicht.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Ophaaldatum")]
+        public DateTime OphaalDatum { get; set; }
 
+        [Required(ErrorMessage = "Lengte moet tussen 260 en 370 liggen.")]
+        [Range(260, 370, ErrorMessage = "Lengte moet tussen 260 en 370 liggen.")]
+        public int Lengte { get; set; }
 
+        [Required(ErrorMessage = "Breedte moet tussen 65 en 85 liggen.")]
+        [Range(65, 85, ErrorMessage = "Breedte moet tussen 65 en 85 liggen.")]
+        public int Breedte { get; set; }
     }
 
     
